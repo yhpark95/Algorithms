14f956e00adf8f066211cb6fbad1a8a60c4fa968
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2024-01-23 23:06:40 +0900

    [Silver III] Title: N과 M (5), Time: 352 ms, Memory: 28784 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static boolean[] visited;
+	static int[] seq;
+	static int[] values;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		seq = new int[M + 1];
+		visited = new boolean[N + 1];
+		values = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			values[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(values);
+		backtracking(N, M, 1);
+		System.out.println(sb);
+	}
+
+	private static void backtracking(int N, int M, int length) {
+		if (length > M) {
+			for (int i = 1; i <= M; i++) {
+				sb.append(seq[i]).append(" ");
+			}
+			sb.deleteCharAt(sb.length() - 1);
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			if(!visited[i]){
+				visited[i] = true;
+				seq[length] = values[i];
+				backtracking(N,M,length+1);
+				visited[i] = false;
+			}
+		}
+	}
+}
